2fa4dd1c6c625a8ebb3573daee48d947a3d1c572
Diego Reis <diegoluis200302@gmail.com>
2023-11-07 23:44:49 -0300

    Update duplicateImage.asm

--- Arquivos/Duplicar Imagem/duplicateImage.asm
+++ Arquivos/Duplicar Imagem/duplicateImage.asm
@@ -24,41 +24,12 @@ include \masm32\macros\macros.asm
     original_fileBuffer db 0H
 
 .code
-
-    _Copia_Primeiros_54_Bytes:
-        ;Prologo da subrotina --------
-        push ebp
-        mov ebp, esp
-   
-        ;eax=param
-        mov eax, DWORD PTR[ebp+8]
-
-        _Clamp_Compara:
-            cmp eax, 0H
-            jl _Clamp_Menor
-            cmp eax, 0FFH ;255
-            jg _Clamp_Maior
-            jmp _Clamp_Return
-
-        _Clamp_Menor:
-            xor eax, eax ;eax = 0
-            jmp _Clamp_Compara
-
-        _Clamp_Maior:
-            mov eax, 255
-            jmp _Clamp_Compara
-
-        ;Epilogo da subrotina --------   
-        _Clamp_Return:
-            mov esp, ebp
-            pop ebp
-            ret ; desaloca parametro
 start:
     ; Abre arquivo original em depois solicita modo leitura
     invoke CreateFile, addr original_fileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
     mov original_fileHandle, eax    
 
-    ; Abre arquivo cópia em modo escrever
+    ; Abre arquivo cÃ³pia em modo escrever
     invoke CreateFile, addr copy_fileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
     mov copy_fileHandle, eax
 
@@ -71,7 +42,7 @@ start:
         cmp original_readCount, 0
         je _Exit_Program
         
-        ;Escreve buffer lido no arquivo cópia
+        ;Escreve buffer lido no arquivo cÃ³pia
         invoke WriteFile, copy_fileHandle, addr original_fileBuffer, read_buffer_size, addr copy_writeCount, NULL ; Escreve buffer_size bytes do arquivo 
 
         jne _Copy_Loop
